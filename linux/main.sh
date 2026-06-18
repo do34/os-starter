6b1bc8d0c5aa8313ec19ba3459e6da14a1ecceee
D. Orlowski <do34spm@gmail.com>
2026-06-18 06:55:52 +0200

    next

--- linux/main.sh
+++ linux/main.sh
@@ -5,6 +5,17 @@ git config --global user.email "do34spm@gmail.com"
 # install pi
 curl -fsSL https://pi.dev/install.sh | sh
 
+# nerd font
+# 1. Create the user font directory if it doesn't exist
+mkdir -p ~/.local/share/fonts
+
+# 2. Download and extract the latest JetBrains Mono Nerd Font
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz | tar --extract --xz --directory ~/.local/share/fonts
+
+# 3. Refresh the system font cache
+fc-cache -fv
+
+
 # Enable Copr repository for yazi                                            
 sudo dnf copr enable lihaohong/yazi                                          
                                                                               
